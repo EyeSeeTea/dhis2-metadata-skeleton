f81d420aa44c8e29e7f45df5474cf5742ab87d8e
Jorge Muñoz Martínez <125618156+JorgeMMdev@users.noreply.github.com>
2025-05-23 13:29:11 +0200

    clean unused components

--- src/webapp/pages/landing/LandingPage.tsx
+++ src/webapp/pages/landing/LandingPage.tsx
@@ -1,43 +1,5 @@
-import React, { useState } from "react";
-import DiffViewer from "$/webapp/components/DiffViewer";
+import React from "react";
 
 export const LandingPage: React.FC = React.memo(() => {
-
-
-    const [showDiff, setShowDiff] = useState(false);
-
-    const originalObject = {
-        name: "John",
-        age: 30,
-        email: "john@example.com",
-    };
-
-    const updatedObject = {
-        name: "John",
-        age: 31,
-        email: "john.doe@example.com",
-    };
-
-  
-    function MyButton() {
-        return (
-            <button onClick={() => setShowDiff(prev => !prev)}>
-                {showDiff ? "Ocultar diferencias" : "Comparar ficheros"}
-            </button>
-        );
-    }
-
-    return (
-        <>
-       
-
-            <MyButton />
-
-            {showDiff && (
-                <div style={{ marginTop: "1rem" }}>
-                    <DiffViewer original={originalObject} updated={updatedObject} />
-                </div>
-            )}
-        </>
-    );
+    return <></>;
 });
