66c746279419d1f7e24d8418b5ea9af10d30c672
Chukwudumebi Onwuli <37223065+deeonwuli@users.noreply.github.com>
2025-09-15 16:24:22 +0100

    refactor: change JSONContent from class to interface and update related methods

--- src/domain/entities/JSONContent.ts
+++ src/domain/entities/JSONContent.ts
@@ -4,26 +4,29 @@ export type JSONPrimitive = string | number | boolean | null;
 export type JSONValue = JSONPrimitive | JSONContent | JSONArray;
 export type JSONArray = JSONValue[];
 
-export class JSONContent {
+export interface JSONContent {
     [key: string]: JSONValue;
-
-    static isValidJSON(jsonContent: Maybe<JSONContent>): jsonContent is JSONContent {
-        return jsonContent !== undefined && this.isObject(jsonContent);
-    }
-
-    static isArray(jsonContent: Maybe<JSONValue>): jsonContent is JSONArray {
-        return Array.isArray(jsonContent);
-    }
-
-    static isObject(jsonContent: Maybe<JSONValue>): jsonContent is JSONContent {
-        return (
-            typeof jsonContent === "object" && jsonContent !== null && !Array.isArray(jsonContent)
-        );
-    }
-
-    static isPrimitive(jsonContent: Maybe<JSONValue>): jsonContent is JSONPrimitive {
-        return (
-            jsonContent !== undefined && !this.isArray(jsonContent) && !this.isObject(jsonContent)
-        );
-    }
 }
+
+export function isValidJSON(jsonContent: Maybe<JSONContent>): jsonContent is JSONContent {
+    return jsonContent !== undefined && isJSONContent(jsonContent);
+}
+
+export function isJSONArray(jsonContent: Maybe<JSONValue>): jsonContent is JSONArray {
+    return Array.isArray(jsonContent);
+}
+
+export function isJSONContent(jsonContent: Maybe<JSONValue>): jsonContent is JSONContent {
+    return typeof jsonContent === "object" && jsonContent !== null && !Array.isArray(jsonContent);
+}
+
+export function isJSONPrimitive(jsonContent: Maybe<JSONValue>): jsonContent is JSONPrimitive {
+    return jsonContent !== undefined && !isJSONArray(jsonContent) && !isJSONContent(jsonContent);
+}
+
+export const JSONContent = {
+    isValidJSON: isValidJSON,
+    isArray: isJSONArray,
+    isObject: isJSONContent,
+    isPrimitive: isJSONPrimitive,
+};
